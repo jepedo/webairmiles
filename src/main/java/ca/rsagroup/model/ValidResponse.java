8de30ae91ae995d60f68a72c0eb9647306f97ebe
horatiu_muresan <horatiu.muresan@rsagroup.ca>
2013-11-22 10:30:56 -0500

    validation messages

--- src/main/java/ca/rsagroup/model/ValidResponse.java
+++ src/main/java/ca/rsagroup/model/ValidResponse.java
@@ -1,6 +1,12 @@
 package ca.rsagroup.model;
 
-public class ValidResponse {
+import java.io.Serializable;
+
+public class ValidResponse implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String policy;
 	private String policyDate;
 	private String status;
--- src/main/java/ca/rsagroup/model/ValidResponses.java
+++ src/main/java/ca/rsagroup/model/ValidResponses.java
@@ -1,10 +1,15 @@
 package ca.rsagroup.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class ValidResponses {
+public class ValidResponses implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private List<ValidResponse> responses;
 
 	public List<ValidResponse> getResponses() {
